938ffd8c527de5f0ec2e1de257a3bca7f1acd7e6
Ion Dormenco <max_i.dormenco@tjip.com>
2019-02-26 13:15:23 +0200

    added .net core version of benchmarks

---
+++ sln/Benchmarks.Core/Branching/AbsSumBenchmark.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks.Core.Branching
+{
+	public class AbsSumBenchmark
+	{
+		private int[] regularData;
+		private int[] sortedData;
+		private int[] shuffledData;
+
+		private AbsSum absSum;
+
+		public int Size = 4 * 1024;
+
+		[Params("10", "1101001001")]
+		public string Pattern;
+
+		[GlobalSetup]
+		public void GlobalSetup()
+		{
+			absSum = new AbsSum();
+			regularData = Utils.FillByPattern(Pattern, Size, 1, -1);
+			sortedData = Utils.SortedCopyOf(regularData);
+			shuffledData = Utils.ShuffledCopyOf(regularData);
+		}
+
+		[Benchmark]
+		public int BranchRegular()
+		{
+			return absSum.AbsSumBranch(regularData);
+		}
+
+		[Benchmark]
+		public int BranchSorted()
+		{
+			return absSum.AbsSumBranch(sortedData);
+		}
+
+		[Benchmark]
+		public int BranchShuffled()
+		{
+			return absSum.AbsSumBranch(shuffledData);
+		}
+
+		[Benchmark]
+		public int BranchlessRegular()
+		{
+			return absSum.AbsSumBranchless(regularData);
+		}
+
+		[Benchmark]
+		public int BranchlessSorted()
+		{
+			return absSum.AbsSumBranchless(sortedData);
+		}
+
+		[Benchmark]
+		public int BranchlessShuffled()
+		{
+			var absSumBranchless = absSum.AbsSumBranchless(shuffledData);
+			if (absSumBranchless != Size)
+			{
+				throw new Exception("upssss");
+			}
+			return absSumBranchless;
+		}
+	}
+}
---
+++ sln/Benchmarks.Core/ManualUnrollBreakingChains/IntMul.cs
@@ -0,0 +1,67 @@
+﻿namespace Benchmarks.Core.ManualUnrollBreakingChains
+{
+	public class IntMul
+	{
+		public int Mul(int[] array)
+		{
+			int p = 1;
+			for (int i = 0; i < array.Length; i++)
+			{
+				p *= array[i];
+			}
+			return p;
+		}
+
+		public int Mul2Accumulators(int[] array)
+		{
+			int p0 = 1;
+			int p1 = 1;
+			for (int i = 0; i < array.Length; i += 2)
+			{
+				p0 *= array[i];
+				p1 *= array[i + 1];
+			}
+			return p0 * p1;
+		}
+
+		public int Mul4Accumulators(int[] array)
+		{
+			int p0 = 1;
+			int p1 = 1;
+			int p2 = 1;
+			int p3 = 1;
+			for (int i = 0; i < array.Length; i += 4)
+			{
+				p0 *= array[i];
+				p1 *= array[i + 1];
+				p2 *= array[i + 2];
+				p3 *= array[i + 3];
+			}
+			return (p0 * p1) * (p2 * p3);
+		}
+
+		public int Mul8Accumulators(int[] array)
+		{
+			int p0 = 1;
+			int p1 = 1;
+			int p2 = 1;
+			int p3 = 1;
+			int p4 = 1;
+			int p5 = 1;
+			int p6 = 1;
+			int p7 = 1;
+			for (int i = 0; i < array.Length; i += 8)
+			{
+				p0 *= array[i];
+				p1 *= array[i + 1];
+				p2 *= array[i + 2];
+				p3 *= array[i + 3];
+				p4 *= array[i + 4];
+				p5 *= array[i + 5];
+				p6 *= array[i + 6];
+				p7 *= array[i + 7];
+			}
+			return ((p0 * p1) * (p2 * p3)) * ((p4 * p5) * (p6 * p7));
+		}
+	}
+}
